1f93bd49ce96dc807d1c805ca5063d951fd621fb
Oliver Foggin <oliverfoggin@me.com>
2021-07-10 17:44:16 +0100

    Initial Project

---
+++ SpaceX/SpaceX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpaceX
+//
+//  Created by Foggin, Oliver (Developer) on 10/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpaceX/SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Foggin, Oliver (Developer) on 10/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
