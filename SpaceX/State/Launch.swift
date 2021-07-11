1c0b6ea370ed21a72e75cd438fb4e4dcad85363e
Oliver Foggin <oliverfoggin@me.com>
2021-07-11 16:27:56 +0100

    Refactored LaunchViewState logic

--- SpaceX/State/Launch.swift
+++ SpaceX/State/Launch.swift
@@ -20,9 +20,60 @@ struct Launch: Identifiable, Equatable {
 }
 
 struct LaunchState: Identifiable, Equatable {
+    enum MissionSuccess {
+        case success, failure, unknown
+        
+        init(success: Bool?) {
+            switch success {
+            case .none:
+                self = .unknown
+            case .some(true):
+                self = .success
+            case .some(false):
+                self = .failure
+            }
+        }
+    }
+    
     var id: String { self.launch.id }
     var launch: Launch
     var rocket: Rocket?
+    
+    let missionName: String
+    let dateTime: String
+    let rocketInfo: String
+    let days: Int
+    let successful: MissionSuccess
+    
+    init(launch: Launch, rocket: Rocket?) {
+        self.launch = launch
+        self.rocket = rocket
+        self.missionName = launch.missionName
+        self.dateTime = "\(Self.dateFormatter.string(from: launch.launchDate)) at \(Self.timeFormatter.string(from: launch.launchDate))"
+        if let rocket = rocket {
+            self.rocketInfo = "\(rocket.name) / \(rocket.type)"
+        } else {
+            self.rocketInfo = "Unknown"
+        }
+        self.days = 100
+        self.successful = .init(success: launch.success)
+    }
+}
+
+extension LaunchState {
+    static let dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .none
+        return df
+    }()
+    
+    static let timeFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .none
+        df.timeStyle = .short
+        return df
+    }()
 }
 
 extension Launch: Decodable {
--- SpaceX/Views/LaunchView.swift
+++ SpaceX/Views/LaunchView.swift
@@ -9,94 +9,39 @@ import SwiftUI
 import ComposableArchitecture
 
 struct LaunchView: View {
-    struct ViewState: Equatable {
-        enum MissionSuccess {
-            case success, failure, unknown
-            
-            init(success: Bool?) {
-                switch success {
-                case .none:
-                    self = .unknown
-                case .some(true):
-                    self = .success
-                case .some(false):
-                    self = .failure
-                }
-            }
-        }
-        
-        let missionName: String
-        let dateTime: String
-        let rocketInfo: String
-        let days: Int
-        let successful: MissionSuccess
-        
-        init(state: LaunchState) {
-            self.missionName = state.launch.missionName
-            self.dateTime = "\(Self.dateFormatter.string(from: state.launch.launchDate)) at \(Self.timeFormatter.string(from: state.launch.launchDate))"
-            if let rocket = state.rocket {
-                self.rocketInfo = "\(rocket.name) / \(rocket.type)"
-            } else {
-                self.rocketInfo = "Unknown"
-            }
-            self.days = 100
-            self.successful = .init(success: state.launch.success)
-        }
-    }
-    
     let store: Store<LaunchState, LaunchAction>
-    @ObservedObject var viewStore: ViewStore<ViewState, LaunchAction>
-    
-    init(store: Store<LaunchState, LaunchAction>) {
-      self.store = store
-      self.viewStore = ViewStore(self.store.scope(state: ViewState.init(state:)))
-    }
     
     var body: some View {
-        HStack(alignment: .firstTextBaseline) {
-            Image(systemName: "paperplane.fill")
-            VStack(alignment: .leading) {
-                Text("Mission:")
-                Text("Date/time:")
-                Text("Rocket:")
-                Text("Days \(self.viewStore.days < 0 ? "since" : "from") now:")
+        WithViewStore(self.store) { viewStore in
+            HStack(alignment: .firstTextBaseline) {
+                Image(systemName: "paperplane.fill")
+                VStack(alignment: .leading) {
+                    Text("Mission:")
+                    Text("Date/time:")
+                    Text("Rocket:")
+                    Text("Days \(viewStore.days < 0 ? "since" : "from") now:")
+                }
+                VStack(alignment: .leading) {
+                    Text(viewStore.missionName)
+                    Text(viewStore.dateTime)
+                    Text(viewStore.rocketInfo)
+                    Text("\(viewStore.days < 0 ? "-" : "+")\(viewStore.days)")
+                }
+                Spacer()
+                switch viewStore.successful {
+                case .success:
+                    Image(systemName: "checkmark")
+                case .failure:
+                    Image(systemName: "xmark")
+                case .unknown:
+                    Image(systemName: "questionmark")
+                }
             }
-            VStack(alignment: .leading) {
-                Text(self.viewStore.missionName)
-                Text(self.viewStore.dateTime)
-                Text(self.viewStore.rocketInfo)
-                Text("\(self.viewStore.days < 0 ? "-" : "+")\(self.viewStore.days)")
+            .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+            .onTapGesture {
+                viewStore.send(.launchTapped)
             }
-            Spacer()
-            switch self.viewStore.successful {
-            case .success:
-                Image(systemName: "checkmark")
-            case .failure:
-                Image(systemName: "xmark")
-            case .unknown:
-                Image(systemName: "questionmark")
-            }
-        }
-        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-        .onTapGesture {
-            viewStore.send(.launchTapped)
         }
     }
-}
-
-extension LaunchView.ViewState {
-    static let dateFormatter: DateFormatter = {
-        let df = DateFormatter()
-        df.dateStyle = .short
-        df.timeStyle = .none
-        return df
-    }()
-    
-    static let timeFormatter: DateFormatter = {
-        let df = DateFormatter()
-        df.dateStyle = .none
-        df.timeStyle = .short
-        return df
-    }()
 }
