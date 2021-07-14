d283149f748f6717c4cc107fb9218685ada927fd
Oliver Foggin <oliverfoggin@me.com>
2021-07-14 20:56:59 +0100

    Moved Calendar extension

--- SpaceX/View Models/LaunchViewModel.swift
+++ SpaceX/View Models/LaunchViewModel.swift
@@ -69,17 +69,6 @@ struct LaunchViewModel: Identifiable, Equatable {
     }
 }
 
-extension Calendar {
-    func numDaysBetween(_ date1: Date, _ date2: Date) -> Int {
-        let fromDate = startOfDay(for: date1)
-        let toDate = startOfDay(for: date2)
-        
-        let numberDays = dateComponents([.day], from: fromDate, to: toDate)
-        
-        return numberDays.day ?? 0
-    }
-}
-
 extension LaunchViewModel {
     static let dateFormatter: DateFormatter = {
         let df = DateFormatter()
